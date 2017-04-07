1698ab30318fbb95d79418f366c3fc98bf3c621b
Paulo Fernando <pfasba@gmail.com>
2017-04-06 21:25:46 -0300

    Messages being sent successfully

--- app/src/main/kotlin/site/paulo/localchat/ui/room/RoomActivity.kt
+++ app/src/main/kotlin/site/paulo/localchat/ui/room/RoomActivity.kt
@@ -1,25 +1,26 @@
 package site.paulo.localchat.ui.room
 
 import android.os.Bundle
-import android.support.design.widget.AppBarLayout
 import android.support.v7.widget.Toolbar
 import android.widget.Button
+import android.widget.EditText
 import butterknife.BindView
 import butterknife.ButterKnife
-import org.jetbrains.anko.startActivity
 import site.paulo.localchat.R
 import site.paulo.localchat.data.model.chatgeo.Chat
+import site.paulo.localchat.data.model.chatgeo.ChatMessage
 import site.paulo.localchat.ui.base.BaseActivity
-import site.paulo.localchat.ui.dashboard.DashboardActivity
 import site.paulo.localchat.ui.signin.RoomContract
 import site.paulo.localchat.ui.signin.RoomPresenter
-import java.security.acl.Group
 import javax.inject.Inject
 
 class RoomActivity : BaseActivity() , RoomContract.View {
 
     @Inject
     lateinit var presenter: RoomPresenter
+
+    @BindView(R.id.messageTxt)
+    lateinit var messageText: EditText
 
     @BindView(R.id.sendBtn)
     lateinit var sendBtn: Button
@@ -41,12 +42,16 @@ class RoomActivity : BaseActivity() , RoomContract.View {
         presenter.attachView(this)
 
         sendBtn.setOnClickListener {
-            startActivity<DashboardActivity>()
+            presenter.sendMessage(ChatMessage("Name test", "Message test"))
         }
     }
 
     override fun showMessages() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun cleanMessageField() {
+        messageText.text.clear()
     }
 
     override fun onDestroy() {
--- app/src/main/kotlin/site/paulo/localchat/ui/room/RoomPresenter.kt
+++ app/src/main/kotlin/site/paulo/localchat/ui/room/RoomPresenter.kt
@@ -1,13 +1,20 @@
 package site.paulo.localchat.ui.signin
 
+import com.google.firebase.database.FirebaseDatabase
+import site.paulo.localchat.data.model.chatgeo.ChatMessage
 import javax.inject.Inject
 
 class RoomPresenter
 @Inject
-constructor() : RoomContract.Presenter() {
+constructor(private val firebase: FirebaseDatabase) : RoomContract.Presenter() {
 
     override fun loadMessages() {
         view.showMessages()
+    }
+
+    override fun sendMessage(message: ChatMessage) {
+        firebase.getReference("chats").child("General").child("messages").push().setValue(message)
+        view.cleanMessageField()
     }
 
 }
