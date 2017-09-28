c2791f362d3259f99c59a2d46703acef80b27931
Paulo Fernando <pfernando@valore.com>
2017-09-27 22:28:22 -0300

    Adding app version in About Screen

--- app/src/main/kotlin/site/paulo/localchat/ui/about/AboutActivity.kt
+++ app/src/main/kotlin/site/paulo/localchat/ui/about/AboutActivity.kt
@@ -6,6 +6,11 @@ import kotlinx.android.synthetic.main.activity_about.*
 import org.jetbrains.anko.ctx
 
 import site.paulo.localchat.R
+import android.content.pm.PackageManager
+import android.R.attr.versionName
+import android.content.pm.PackageInfo
+
+
 
 class AboutActivity : AppCompatActivity() {
 
@@ -13,6 +18,14 @@ class AboutActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
 
-        appNameAboutTxt.text = ctx.getApplicationInfo().loadLabel(ctx.getPackageManager())
+        appNameAboutTxt.text = ctx.getApplicationInfo().loadLabel(ctx.packageManager)
+
+        try {
+            val pInfo = this.packageManager.getPackageInfo(packageName, 0)
+            appVersionAboutTxt.text = pInfo.versionName
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+
     }
 }
